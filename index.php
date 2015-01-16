6231d47372d660c413eee0d62551e3132fee7a88
BozzaCoon <siddy_88@gmx.de>
2015-01-16 09:19:42 +0100

    change html to php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head lang='de'>";
+echo "    <meta charset='UTF-8'>";
+echo "    <title> I have not Title</title>";
+echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+echo "</head>";
+echo "<body id='bod'>";
+echo "<div id='header'>";
+    echo "<h2 style='color:white;'>Using CSS.</h2>";
+echo "</div>";
+echo "<div id='text'> only a small test</div>";
+echo "</body>";
+echo "</html>";
+
+?>
