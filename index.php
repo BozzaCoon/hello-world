65f474f0cd579d2e66a98570ca0f83e02ea7ceb3
BozzaCoon <siddy_88@gmx.de>
2015-01-23 09:17:06 +0100

    rapair the php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ echo "<body id='bod'>";
 echo "<div id='header'>";
     echo "<h2 style='color:white;'>Using CSS and change someting</h2>";
     echo "<div>Bohja more changes.</div>";
-    echo "<div>Bohja more changes. again...</div>"
+    echo "<div>Bohja more changes. again...</div>";
 echo "</div>";
 echo "<div id='text'> only a small test</div>";
 echo "</body>";
