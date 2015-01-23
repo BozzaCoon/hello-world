8b01d8bc31887dfc177aa7ffaf5aeb0d8d7167b7
BozzaCoon <siddy_88@gmx.de>
2015-01-23 09:19:43 +0100

    updating again

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ echo "<div id='header'>";
     echo "<h2 style='color:white;'>Using CSS and change someting</h2>";
     echo "<div>Bohja more changes.</div>";
     echo "<div>Bohja more changes. again...</div>";
+    echo "<div>OK that works... not realy sure why.</div>";
 echo "</div>";
 echo "<div id='text'> only a small test</div>";
 echo "</body>";
