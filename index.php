14edf4d1d213e90b91694e4700dab6deada48ce9
BozzaCoon <siddy_88@gmx.de>
2015-01-26 11:52:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ echo "<div id='header'>";
     echo "<div>now some lines of text. some...</div>";
     echo "<div>change my bat file and look for working</div>";
     echo "<div>change to test the new git Installation.</div>";
+    echo "<div>more interesting information</div>";
 echo "</div>";
 echo "<div id='text'> only a small test</div>";
 echo "</body>";
