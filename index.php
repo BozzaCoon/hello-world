f5c93c42beaa96b827c57fd80c255e6db0e7c184
BozzaCoon <siddy_88@gmx.de>
2015-01-26 12:01:33 +0100

    i have to do it

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ echo "<div id='header'>";
     echo "<div>change my bat file and look for working</div>";
     echo "<div>change to test the new git Installation.</div>";
     echo "<div>more interesting information</div>";
-    echo "<div>more interesting ... information</div>";
 echo "</div>";
 echo "<div id='text'> only a small test</div>";
 echo "</body>";
